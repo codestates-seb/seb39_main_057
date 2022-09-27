5095ed02f9bc27836e448c096012330f61cc9794
kingswingsex <kingswingsex@gmail.com>
2022-09-27 11:48:54 +0900

    Chore : Dto 설명 추가

--- server/src/main/java/com/courseori/server/member/dto/MemberDto.java
+++ server/src/main/java/com/courseori/server/member/dto/MemberDto.java
@@ -9,7 +9,9 @@ import lombok.Setter;
 import org.springframework.stereotype.Service;
 
 public class MemberDto {
-
+    //patch 만을 위한 DTO입니다.
+    //참고할 게 있다면 Service에 Update와
+    //Controller에 Update 로직을 참고하세요
     @NoArgsConstructor
     @Getter
     @Setter
